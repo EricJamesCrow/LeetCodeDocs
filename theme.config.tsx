6b908a83292bf009d0a7757868db96ed6036ed34
EricJamesCrow <EricCrow@pm.me>
2023-08-06 15:31:55 -0700

    initial commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>LeetCodeDocs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/EricJamesCrow',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Eric's LeetCode Documentation",
   },
 }
 
